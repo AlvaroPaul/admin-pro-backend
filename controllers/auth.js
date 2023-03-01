490838821af751d343951aec322702615df7d70d
paul <alvaropaul07@hotmail.com>
2023-03-01 10:32:27 -0500

    Fin seccion 18

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/jwt");
 const { googleVerify } = require("../helpers/google-verify");
+const { getMenuFrontEnd } = require("../helpers/menu-frontend");
 
 const login = async( req, res = response) => {
 
@@ -35,8 +36,9 @@ const login = async( req, res = response) => {
 
         res.json({
             ok: true,
-            token
-        })
+            token,
+            menu: getMenuFrontEnd( usuarioDB.role )
+        });
 
     } catch (error) {
         console.log(error);
@@ -78,7 +80,8 @@ const googleSignIn = async( req, res = response) => {
         res.json({
             ok: true,
             email, name, picture,
-            token
+            token,
+            menu: getMenuFrontEnd( usuario.role )
         });
 
     } catch (error) {
@@ -104,7 +107,8 @@ const renewToken = async (req, res = response) => {
     res.json({
         ok: true,
         token,
-        usuario
+        usuario,
+        menu: getMenuFrontEnd( usuario.role )
     });
 
 }
